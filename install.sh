6705ffb031686feafb96591dcd2362cc0b62cb75
tiancaiV <wifiway@163.com>
2023-01-20 18:36:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sed -i '/custom_feeds/d' /etc/opkg/customfeeds.conf
-echo "src/gz custom_feeds  https://cdn.jsdelivr.net/gh/tiancaiV/2500/MT2500" >>/etc/opkg/customfeeds.conf
+echo "src/gz custom_feeds https://op.supes.top/packages/aarch64_cortex-a53/" >>/etc/opkg/customfeeds.conf
 opkg update
 action="$1"
 case "${action}" in
@@ -11,9 +11,7 @@ passwall)
 	opkg install luci-i18n-base-zh-cn luci-app-passwall luci-i18n-passwall-zh-cn luci-compat luci-lib-ipkg
 	;;
 openclash)
-	wget -qO- https://cdn.jsdelivr.net/gh/m0eak/Install-Openclash-for-Axt1800/install_clash.sh | sh
-	;;
-*)
+	
 	opkg install luci-i18n-base-zh-cn luci-app-passwall luci-i18n-passwall-zh-cn luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn luci-compat luci-lib-ipkg
 	wget -qO- https://cdn.jsdelivr.net/gh/m0eak/Install-Openclash-for-Axt1800/install_clash.sh | sh
 	;;
